57e831383786d550c57c581ea63c7849525aa7b8
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2024-02-22 15:32:58 -0500

    Agregadas clases City y Person en el paquete pojo

---
+++ src/pojo/City.java
@@ -0,0 +1,27 @@
+package pojo;
+
+public class City {
+    private String daneCode;
+    private String name;
+
+    public City(String daneCode, String name) {
+        this.daneCode = daneCode;
+        this.name = name;
+    }
+
+    public String getDaneCode() {
+        return daneCode;
+    }
+
+    public void setDaneCode(String daneCode) {
+        this.daneCode = daneCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
