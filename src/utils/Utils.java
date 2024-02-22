aeceb56d7ade89b9c2afdc026f396052791ac21b
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2024-02-22 15:57:46 -0500

    Ordenamiento de Paquetes, Json personas agregado, creacion de clase Utils
    Se ordenaron los paquetes que contenian las librerias
    Se agrego el archivo json con personas
    Se creo la clase Utils para codigo reutilizable

---
+++ src/utils/Utils.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Utils {
+    public static final String REDMESSAGE = "\033[31m";
+    public static final String GREENMESSAGE = "\033[32m";
+    public static final String RESET = "\u001B[0m";
+    public static final String PURPLEMESSAGE = "\033[35m";
+    public static final String YELLOWMESSAGE = "\033[33m";
+    public static final String BLUEMESSAGE = "\033[34m";
+    public static final String CYANMESSAGE = "\033[36m";
+}
